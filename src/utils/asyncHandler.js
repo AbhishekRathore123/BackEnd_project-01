7bd2f57ab12a77e59cbdc21285d65be31dab7b80
Abhishek Rathore <abhishek1122rathore@gmail.com>
2026-06-18 16:53:01 +0530

    make utils files

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+// This mathod are used to async task
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err)=>next(err))
+    }
+}
+
+export { asyncHandler }
+
+
+
+//! Try Catch ----------
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
