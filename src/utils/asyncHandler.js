e12317770f2266ed82c30c8f7d21d9805dbaf4cc
Abhishek Rathore <abhishek1122rathore@gmail.com>
2026-06-19 15:52:16 +0530

    all setup configurd

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 // This mathod are used to async task
 
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
+    return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).catch((err)=>next(err))
     }
 }
